a15857c78050353e4b0496274d0251dcb84b3f51
Is6 <bakers4869@gmail.com>
2018-01-20 01:12:33 +0900

    機能追加_試し
    描画　ON　OFF
    テクスチャ張り替え
    カード　種類　フラグ　の取得
    及び　変更の適用
    フィールド配置の変更
    カード　手札　のプレハブ作成
    　

---
+++ Assets/deal_card.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deal_card : MonoBehaviour {//カードを手札に配る
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
