4097321d781882a9a91710a2a3187824ca7b13f6
ap0sec <apo.t.laboratory@gmail.com>
2018-01-23 22:29:05 +0900

    [Add] Card class

---
+++ Assets/Card.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    public Material teacher;
+    public Material student;
+    public Material retination;
+    public Material jobee;
+
+    int type = 0; //0:teacher 1:student 2:retination 3:jobee
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            type++;
+            if (type == 4)
+            {
+                type = 0;
+            }
+        }
+
+            switch (type)
+        {
+            case 0:
+                this.GetComponent<Renderer>().material = teacher;
+                break;
+            case 1:
+                this.GetComponent<Renderer>().material = student;
+                break;
+            case 2:
+                this.GetComponent<Renderer>().material = retination;
+                break;
+            case 3:
+                this.GetComponent<Renderer>().material = jobee;
+                break;
+        }
+		
+	}
+}
