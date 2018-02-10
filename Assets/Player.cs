124b0288e5a6599a8dbef4f7638c10ef7f10d2bd
nitkc <yo235302353@yahoo.co.jp>
2018-02-10 23:36:44 +0900

    Super bigbang ultimate update 2018!
    ○Set_bet()書いたよ
    　・関数呼び出しでGUIが現れるよ
    　・GUIに数字を入れてspaceキーを押下すると値がセットされるよ
    　・そのあとGUIはどっかに消えるよ

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+
 
 public class Player : MonoBehaviour {
 
@@ -9,16 +12,16 @@ public class Player : MonoBehaviour {
     public int bet = 0;
 
     //引数は自由に追加してもらって構いません
-    //その場合はsampleに従う形で何が必要なのか書いてください
+    //その場合はsampleに従う形で何が必要なのか書いてくださ
     //戻り値は基本的にvoidで mainから叩くときに何か欲しくなったらこっちからいじるかも
     //marge・conflict消化は小高がやります
 
 
     /* 変数宣言が必要な場合は適宜ここから*/
-
-
-
-
+	private Text ger;
+	GameObject hoge;
+	GameObject hoge2;
+	GameObject hoge3;
     /*ここまで*/
 
     //sample
@@ -34,12 +37,19 @@ public class Player : MonoBehaviour {
 
     // Use this for initialization 一応残す
 	void Start () {
-		
+		hoge2 = GameObject.Find("GameObject");
+		hoge2.GetComponent<Player>().Set_Bet();
 	}
 	
 	// Update is called once per frame 一応残す
 	void Update () {
-		
+
+	//トリガーがわからないので, 便宜的にスペースキーが押されたタイミングでSet_Bet呼び出し.
+		//if(Input.GetKey(KeyCode.Space)){
+		//	hoge2 = GameObject.Find("GameObject");
+		//	hoge2.GetComponent<Player>().Set_Bet();
+		//}
+
 	}
 
     void Set_hand(int[] type) //デッキからカードを引く時用メゾット 大浦
@@ -49,6 +59,25 @@ public class Player : MonoBehaviour {
 
     void Set_Bet() //ベット用メゾット 宇津木
     {
+	//便宜的に, update()から呼び出し.
+
+	hoge = GameObject.Find("GameObject/bet/Canvas/InputField/Text");
+	hoge3 = GameObject.Find("GameObject/bet/Canvas/InputField");
+
+	//ベット用GUI表示.
+	hoge3.GetComponent<Image>().enabled = true;
+	hoge.GetComponent<Text>().enabled = true;
+	
+	while(Input.GetKey(KeyCode.Space) != true){}
+	//int betに入力値をセット.
+	ger = hoge.GetComponent<Text>();
+	bet = Convert.ToInt32(ger.text);
+	//Debug.Log(bet);
+	
+	//GUI消失.
+	hoge3.GetComponent<Image>().enabled = false;
+	hoge.GetComponent<Text>().enabled = false;
+	
 
     }
 
